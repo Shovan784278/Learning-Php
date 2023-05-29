35ba179d6f739470cdde0f20bccecbde467e65a4
Shovon Nag <Shovanr06@gmail.com>
2023-05-29 23:53:45 +0600

    updated

--- Module15_assignment/projectThree/resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('Layout.app')
-
-@section('title', 'Home Page')
-@section('description', 'this is homepage Description')
-@section('keyowrds','comma, separated, list, of, relevant, keywords')
-
-@section('content')
-   @include('component.deshboardhero')
-@endsection
